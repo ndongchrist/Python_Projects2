5022ccaef95c7e2024fb155d65885146d806c994
Akashiutchiha <christianhonore2003@gmail.com>
2023-02-13 20:18:59 +0100

    feat:Added Exercise 4

---
+++ 50_Coding_Ex/Ex4.py
@@ -0,0 +1,39 @@
+for i in range(0, 13):
+    answer = i * 1
+    print(f"{i} * 1 = {answer}")
+    
+for i in range(0, 13):
+    answer = i * 2
+    print(f"{i} * 2 = {answer}")
+    
+for i in range(0, 13):
+    answer = i * 3
+    print(f"{i} * 3 = {answer}")
+    
+for i in range(0, 13):
+    answer = i * 4
+    print(f"{i} * 4 = {answer}")
+    
+for i in range(0, 13):
+    answer = i * 5
+    print(f"{i} * 5 = {answer}")
+    
+for i in range(0, 13):
+    answer = i * 6
+    print(f"{i} * 6 = {answer}")
+    
+for i in range(0, 13):
+    answer = i * 7
+    print(f"{i} * 7 = {answer}")
+    
+for i in range(0, 13):
+    answer = i * 8
+    print(f"{i} * 8 = {answer}")
+    
+for i in range(0, 13):
+    answer = i * 9
+    print(f"{i} * 9 = {answer}")
+    
+for i in range(0, 13):
+    answer = i * 10
+    print(f"{i} * 10 = {answer}")
