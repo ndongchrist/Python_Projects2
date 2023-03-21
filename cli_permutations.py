06ba901faa53e4dd84ccff2926799c6f062ef8ad
Akashiutchiha <christianhonore2003@gmail.com>
2023-03-21 18:20:11 +0100

    cli tool which perform all forms of permutations

---
+++ cli_permutations.py
@@ -0,0 +1,32 @@
+import itertools
+
+
+# Permutations with repetition
+def permutations_with(n, list):
+    print("\nPermutations with repetition:")
+    for p in itertools.product(list, repeat=n):
+        print(p)
+
+# Permutations without repetition
+def Permutatins_without(n, list):    
+    print("\nPermutations without repetition:")
+    for p in itertools.permutations(list, n):
+        print(p)
+
+if __name__ == '__main__':
+    print("Enter the list of elements separated by space: ")
+    list = input().split()
+    
+    print("Enter the length of permutation: ")
+    lenght = int(input())  
+    
+    descision = int(input("1: With repetition \t 2: Without repetition"))
+    while(descision != 1 or descision != 2):
+        descision = int(input("1: With repetition \t 2: Without repetition"))
+        
+    if(descision == 1):
+        permutations_with(lenght, list)
+    
+    if(descision == 2):
+        Permutatins_without(lenght, list)
+    
