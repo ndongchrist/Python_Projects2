001baebdbd90faff52a9407dbf8304d8e73c6513
Akashiutchiha <christianhonore2003@gmail.com>
2023-03-22 18:53:55 +0100

    feat: Added choice between With or Without repetion

--- cli_permutations.py
+++ cli_permutations.py
@@ -1,52 +1,40 @@
-def permutations():
-    print("Enter the list of elements separated by space: ")
-    lst = input().split()
-    print("Enter the length of permutation: ")
-    n = int(input())
-    
+def permutations_with_rep(n, list):
     # Permutations with repetition.
     print("\nPermutations with repetition:")
-    for i in range(len(lst)**n):
+    for i in range(len(list)**n):
         p = []
         for j in range(n):
-            p.append(lst[i // (len(lst)**j) % len(lst)])
+            p.append(list[i // (len(list)**j) % len(list)])
         print(tuple(p))
-    
+
+def permutations_without_rep(n, list):
     # Permutations without repetition.
     print("\nPermutations without repetition:")
-    used = [False] * len(lst)
+    used = [False] * len(list)
     def permute(p):
         if len(p) == n:
             print(tuple(p))
         else:
-            for i in range(len(lst)):
+            for i in range(len(list)):
                 if not used[i]:
                     used[i] = True
-                    permute(p + [lst[i]])
+                    permute(p + [list[i]])
                     used[i] = False
     permute([])
     
-    # Combinations with repetition.
-    # print("\nCombinations with repetition:")
-    # def combine(p, start):
-    #     if len(p) == n:
-    #         print(tuple(p))
-    #     else:
-    #         for i in range(start, len(lst)):
-    #             combine(p + [lst[i]], i)
-                
-    # combine([], 0)
     
-    # # Combinations without repetition.
-    # print("\nCombinations without repetition:")
-    # def choose(p, start):
-    #     if len(p) == n:
-    #         print(tuple(p))
-    #     else:
-    #         for i in range(start, len(lst)):
-    #             choose(p + [lst[i]], i+1)
-                
-    # choose([], 0)
+print("Enter the list of elements separated by space: ")
+list = input().split()
+print("Enter the length of permutation: ")
+lenght = int(input())
 
-print("Hr")
-permutations()
+descision = int(input("1: With repetition \t 2: Without repetition\n"))
+while(descision != 1 and descision != 2):
+    print("Invalid input")
+    descision = int(input("1: With repetition \t 2: Without repetition\n"))
+    
+if(descision == 1):
+    permutations_with_rep(lenght, list)
+
+if(descision == 2):
+    permutations_without_rep(lenght, list)
