b4713199b7c3a867a282044a5f29eafeeaa274b8
Akashiutchiha <christianhonore2003@gmail.com>
2023-03-22 18:36:19 +0100

    removed the itertools

--- cli_permutations.py
+++ cli_permutations.py
@@ -1,33 +1,52 @@
-import itertools
-
-
-# Permutations with repetition
-def permutations_with(n, list):
-    print("\nPermutations with repetition:")
-    for p in itertools.product(list, repeat=n):
-        print(p)
-
-# Permutations without repetition
-def Permutatins_without(n, list):    
-    print("\nPermutations without repetition:")
-    for p in itertools.permutations(list, n):
-        print(p)
-
-if __name__ == '__main__':
+def permutations():
     print("Enter the list of elements separated by space: ")
-    list = input().split()
-    
+    lst = input().split()
     print("Enter the length of permutation: ")
-    lenght = int(input())  
+    n = int(input())
     
-    descision = int(input("1: With repetition \t 2: Without repetition\n"))
-    while(descision != 1 and descision != 2):
-        print("Invalid input")
-        descision = int(input("1: With repetition \t 2: Without repetition\n"))
-        
-    if(descision == 1):
-        permutations_with(lenght, list)
+    # Permutations with repetition.
+    print("\nPermutations with repetition:")
+    for i in range(len(lst)**n):
+        p = []
+        for j in range(n):
+            p.append(lst[i // (len(lst)**j) % len(lst)])
+        print(tuple(p))
     
-    if(descision == 2):
-        Permutatins_without(lenght, list)
+    # Permutations without repetition.
+    print("\nPermutations without repetition:")
+    used = [False] * len(lst)
+    def permute(p):
+        if len(p) == n:
+            print(tuple(p))
+        else:
+            for i in range(len(lst)):
+                if not used[i]:
+                    used[i] = True
+                    permute(p + [lst[i]])
+                    used[i] = False
+    permute([])
     
+    # Combinations with repetition.
+    # print("\nCombinations with repetition:")
+    # def combine(p, start):
+    #     if len(p) == n:
+    #         print(tuple(p))
+    #     else:
+    #         for i in range(start, len(lst)):
+    #             combine(p + [lst[i]], i)
+                
+    # combine([], 0)
+    
+    # # Combinations without repetition.
+    # print("\nCombinations without repetition:")
+    # def choose(p, start):
+    #     if len(p) == n:
+    #         print(tuple(p))
+    #     else:
+    #         for i in range(start, len(lst)):
+    #             choose(p + [lst[i]], i+1)
+                
+    # choose([], 0)
+
+print("Hr")
+permutations()
